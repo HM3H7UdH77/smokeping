aa455b5cc566507e9d54f4419fb31ed9bca547f4
xieshang <30817148+Xiefengshang@users.noreply.github.com>
2022-02-27 00:46:09 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,5 +4,7 @@ systemctl disable smokeping-slave.service
 systemctl disable smokeping-master.service
 # caddy关闭
 systemctl disable caddy-sp.service
+# spawn-fcgi关闭
+systemctl disable spawn-fcgi
 # smokeping配置清空
 rm -rf /usr/local/smokeping
