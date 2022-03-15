47a82a9df1ad3e07b5702fed39fdc2cbe364b51f
xieshang <30817148+Xiefengshang@users.noreply.github.com>
2022-03-15 11:36:13 +0800

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -1,9 +1,9 @@
-# slave端重启
-systemctl restart smokeping-slave.service
 # spawn-fcgi重启
 systemctl restart spawn-fcgi.service
 # master端重启
 systemctl restart smokeping-master.service
+# slave端重启
+systemctl restart smokeping-slave.service
 # caddy重启
 systemctl restart caddy-sp.service
 
