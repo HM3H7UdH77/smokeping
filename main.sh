64c747118f5230eba663b9534fe5f02fbcc5f03d
xieshang <30817148+Xiefengshang@users.noreply.github.com>
2022-03-15 14:17:30 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -64,7 +64,11 @@ configure() {
 	[[ $error -eq 1 ]] && echo "获取本机 IP 地址失败" && exit 1
 	mkdir -p $caddy_dir
 	wget $origin/Caddyfile -O $caddy_dir/Caddyfile
-	wget https://github.com/caddyserver/caddy/releases/download/v2.4.3/caddy_2.4.3_linux_amd64.tar.gz -O caddy.tar.gz
+	ca_version=$(curl -sL https://api.github.com/repos/caddyserver/caddy/releases | jq -r ".[0].tag_name")
+	ca_version2=${ca_version#*v}
+	
+	#wget https://github.com/caddyserver/caddy/releases/download/v2.4.3/caddy_2.4.3_linux_amd64.tar.gz -O caddy.tar.gz
+	wget https://github.com/caddyserver/caddy/releases/download/$ca_version/caddy_${ca_version2}_`uname -s`_`dpkg --print-architecture`.tar.gz -O caddy.tar.gz
 	rm -rf $(tar xzvf caddy.tar.gz && cp caddy /usr/bin/caddy-sp) caddy.tar.gz
 	wget $origin/tcpping-sp -O /usr/bin/tcpping-sp && chmod +x /usr/bin/tcpping-sp
 	wget $origin/config -O /usr/local/smokeping/etc/config
